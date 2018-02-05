a9eff6975022bc5e2af662b9ba22f45ad715afa7
Luiz Gustavo <lglmoura@gmail.com>
2018-02-05 16:32:42 -0200

    Incluindo chamada do tela de livros

--- app/src/main/java/br/edu/iff/pooa20172/bookstore/activity/Principal.java
+++ app/src/main/java/br/edu/iff/pooa20172/bookstore/activity/Principal.java
@@ -1,7 +1,11 @@
 package br.edu.iff.pooa20172.bookstore.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import br.edu.iff.pooa20172.bookstore.R;
 
@@ -11,5 +15,21 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        setContentView(R.layout.activity_principal);
+        Button livroBT = (Button) findViewById(R.id.btTelaLivros);
+
+        livroBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), LivroActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+    private Context getContext(){
+        return this;
     }
 }
