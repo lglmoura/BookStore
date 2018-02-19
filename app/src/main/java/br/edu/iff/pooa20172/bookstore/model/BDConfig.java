0b1cb8817da01f5aa0eb242c139020d6c5ca2b4a
Luiz Gustavo <lglmoura@gmail.com>
2018-02-18 23:37:06 -0300

    Criando arquivo de Configuração do realm

---
+++ app/src/main/java/br/edu/iff/pooa20172/bookstore/model/BDConfig.java
@@ -0,0 +1,26 @@
+package br.edu.iff.pooa20172.bookstore.model;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+/**
+ * Created by lglmo on 18/02/2018.
+ */
+
+public class BDConfig extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(getApplicationContext());
+
+        RealmConfiguration.Builder builder = new RealmConfiguration.Builder();
+        builder.name("bookstore.realm");
+        builder.schemaVersion(0);
+        builder.deleteRealmIfMigrationNeeded();
+        RealmConfiguration realmConfiguration = builder.build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
